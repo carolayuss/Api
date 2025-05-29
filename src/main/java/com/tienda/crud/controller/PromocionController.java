5a4b038551f749a15358bc07daef2f8ebfbe08be
carolayss <carolayss@gmail.com>
2025-05-29 09:01:08 -0500

    nuevas entidades
    Signed-off-by: carolayss <carolayss@gmail.com>

---
+++ src/main/java/com/tienda/crud/controller/PromocionController.java
@@ -0,0 +1,47 @@
+package com.tienda.crud.controller;
+
+import com.tienda.crud.Model.Promocion;
+import com.tienda.crud.Repository.PromocionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/promocion")
+public class PromocionController {
+
+    private final PromocionRepository promocionRepository;
+
+    public PromocionController(PromocionRepository promocionRepository) {
+        this.promocionRepository = promocionRepository;
+    }
+
+    @GetMapping
+    public List<Promocion> obtenerPromocion(){
+        return promocionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Promocion obtenerPromocionPorId (@PathVariable Long id) {
+        Optional<Promocion> promocion=promocionRepository.findById(id);
+        return promocion.orElse(null);
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Promocion guardarPromocion(@RequestBody Promocion promocion){
+        return promocionRepository.save(promocion);
+    }
+    @PutMapping
+    public Promocion actualizarPromocion(@PathVariable Long id, @RequestBody Promocion promocion) {
+        promocion.setId(id);
+        return promocionRepository.save(promocion);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminarPromocion(@PathVariable Long id){
+        promocionRepository.deleteById(id);
+    }
+}
+
