94d69106e0f84ef1e86b9d8a9f90c6abf0ab0097
carolayuss <carolayuss@gmail.com>
2025-07-25 20:54:20 -0500

    configuración de seguridad
    Signed-off-by: carolayuss <carolayuss@gmail.com>

--- src/main/java/com/tienda/crud/controller/PublicController.java
+++ src/main/java/com/tienda/crud/controller/PublicController.java
@@ -1,4 +1,17 @@
 package com.tienda.crud.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Login")
+@RequiredArgsConstructor
 public class PublicController {
+    @GetMapping("/api")
+    public String api(){
+        return "bienvenido";
+    }
 }
